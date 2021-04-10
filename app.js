80e38b787f4d83ae0d4676083929cb0e5097e273
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-04-10 16:39:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ client.login(process.env.BOT_TOKEN);
 client.on("message", (msg) => {
   if (/I'm/.test(msg) == true || /i'm/.test(msg) == true || /im/.test(msg) == true) {
     var i = msg.content.substring(msg.content.indexOf("m") + 2);
+    Discord.Message({content:"Hello"});
     msg.reply(`nice to meet you ${i}, I am Carmite Bot!`);
+    message.channel.send(`nice to meet you ${i}, I am Carmite Bot!`);
   }
 });
