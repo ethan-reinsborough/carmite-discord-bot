0dbf3d28ac70ea74a6d41695b31eabbaf04a022e
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-04-10 02:55:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ client.login(process.env.BOT_TOKEN);
 
 client.on("message", (msg) => {
   if (/I'm/.test(msg) == true || /i'm/.test(msg) == true || /im/.test(msg) == true) {
-    msg.reply(" Nice to meet you, I am Carmite Bot!");
+    msg.reply(" nice to meet you, I am Carmite Bot!");
   }
 });
