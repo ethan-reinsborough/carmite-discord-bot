5420b1363f37940d9c636af7ef7826954a584e1a
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-04-10 02:19:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const Discord = require("discord.js");
 require("dotenv").config();
 
 const client = new Discord.Client();
-
+const regex = /I'm/;
+const regex2 = /i'm/;
+const regex3 = /im/;
+const found = paragraph.match(regex);
 client.on("ready", () => {
   console.log("Bot is ready");
 });
@@ -12,7 +15,7 @@ client.on("ready", () => {
 client.login(process.env.BOT_TOKEN);
 
 client.on("message", (msg) => {
-  if (msg.content === "Hello") {
-    msg.reply(msg.author.username);
+  if (msg.match(regex) || msg.match(regex2) || msg.match(regex3)) {
+    msg.reply("Nice to meet you, I'm Carmite Bot!");
   }
 });
