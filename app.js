98c8e583c918058e41b78fdf975ea1c1b5b8983e
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-04-10 01:45:00 -0300

    Test

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ client.login(process.env.BOT_TOKEN);
 
 client.on("message", (msg) => {
   if (msg.content === "Hello") {
-    msg.reply(msg.author.displayAvatarURL());
+    msg.reply(msg.author.username);
   }
 });
