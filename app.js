02f421ae44529bf407986236ad9bd7c2edc1ac5d
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-04-10 01:37:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const Discord = require('discord.js');
-require('dotenv').config();
+"use strict";
+
+const Discord = require("discord.js");
+require("dotenv").config();
 
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('Bot is ready');
-  });
+client.on("ready", () => {
+  console.log("Bot is ready");
+});
 
-  client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
 
-  client.on('message', (msg) => {
-    if (msg.content === 'Hello') msg.reply(message.author.displayAvatarURL());
-  });
+client.on("message", (msg) => {
+  if (msg.content === "Hello") {
+    msg.reply(message.author.displayAvatarURL());
+  }
+});
