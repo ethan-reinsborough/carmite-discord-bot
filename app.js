026668379de77591916b16aaca9aeb866d60d94b
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-04-11 01:52:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -53,8 +53,8 @@ client.on("message", async (msg) => {
   }
   if(/gimmedog/.test(msg.content)){
     const res = await fetch('https://dog.ceo/api/breeds/image/random');
-    const { json } = await res.json();
-    msg.channel.send(json);
+    const json = await res.json();
+    msg.channel.send(json[0]);
   }
   if (!msg.author.bot && !(/gimmecat/.test(msg.content))) {
     messageFactories
