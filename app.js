865398c76ddde3e328cb0843e75c052f722c36f1
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-04-11 01:08:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 const fetch = require('node-fetch');
 const Discord = require("discord.js");
-require("dotenv").config();
+const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log("Bot is ready");
