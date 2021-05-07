9c5729ffb7fb2ecd2d87d99ec7543b17c18aff92
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-05-06 23:49:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -338,8 +338,8 @@ client.on("message", async (msg) => {
   //idk
   if(/gowo/.test(msg.content.toLowerCase())){
     let result = msg.content.substr(msg.content.indexOf(" ") + 1);
-    let owo = await neko.sfw.OwOify({text: `${result}`}).then(response => response.json());
-    msg.channel.send(owo['0']);
+    let { owo } = await neko.sfw.OwOify({text: `${result}`}).then(response => response.json());
+    msg.channel.send(owo);
   }
   if(/urban/.test(msg.content.toLowerCase())){
     let words = msg.content.toLowerCase().split(' ');
