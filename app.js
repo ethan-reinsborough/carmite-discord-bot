fdfa9310a4a2711b42bb7b113987033f975483dc
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-05-07 01:48:30 -0300

    Weird

--- app.js
+++ app.js
@@ -495,9 +495,45 @@ if(/nfoxgirl/.test(msg.content.toLowerCase())){
 //#region Snakecord??
 
 //https://www.npmjs.com/package/snakecord
-
+//Might need more dependencies
 if(/gsnake/.test(msg.content.toLowerCase())){
   return snakeGame.newGame(message);
+}
+
+if(/gsnake/.test(msg.content.toLowerCase())){
+  module.exports.run = async (client, message, args) => {
+ 
+    const pokemon = await Spawn().catch(e => {});
+    if (!pokemon) return message.channel.send("Opps! Something went wrong :(");
+    const filter = m => m.author.id === message.author.id;
+  
+    const embed = new MessageEmbed()
+        .setAuthor("Guess the pokemon")
+        .setColor("#FFFF00")
+        .setImage(pokemon.imageURL);
+    
+    await message.channel.send(embed);
+  
+    message.channel.awaitMessages(filter, {
+        max: 1,
+        error: ["time"],
+        time: 15000
+    })
+    .then(collected => {
+        const m = collected.first();
+        if (!m.content || m.content.toLowerCase() !== pokemon.name.toLowerCase()) return message.channel.send(`❌ | Incorrect guess! The answer was **${pokemon.name}**.`);
+        return message.channel.send(`✅ | Correct guess!`);
+    })
+    .catch(() => {
+        message.channel.send(`❌ | You did not answer in time. The correct answer was **${pokemon.name}**!`);
+    });
+  
+  };
+  
+  module.exports.help = {
+    name: "pokemon",
+    aliases: ["guessthepokemon"]
+  };
 }
 
 //#endregion
