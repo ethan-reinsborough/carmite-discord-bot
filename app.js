cc9ab58d8091d3003221c1230b137009914a1b79
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-05-08 20:08:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -563,8 +563,7 @@ let champions = [
 if(/glol/.test(msg.content.toLowerCase())){
   msg.channel.send("test");
   const res = await fetch(`http://ddragon.leagueoflegends.com/cdn/11.9.1/data/en_US/champion.json`).then(response => response.json());
-  var test = Object.values(res);
-  msg.channel.send(test[3][0][1]);
+  msg.channel.send(res['data']['0']);
   //let champList = Object.keys(res.length);
   //msg.channel.send(Object.keys(res['data']).length);
   let randIndex = Math.floor((Math.random() * champList));
