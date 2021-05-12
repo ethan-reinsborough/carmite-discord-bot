59546c58ed361829b5cd0d0eac5c96824a1ee6fd
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-05-12 03:45:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -786,11 +786,16 @@ client.on("message", async (msg) => {
   }
 
   if (/gicon/.test(msg.content.toLowerCase())) {
-    const randIndex = Math.floor(Math.random() * 4970);
+    const res = await fetch(
+      `http://ddragon.leagueoflegends.com/cdn/11.10.1/data/en_US/item.json`
+    ).then((response) => response.json());
+    const keys = Object.keys(res["data"]);
+    const randIndex = Math.floor(Math.random() * keys.length);
+    const iconID = keys[randIndex];
     const embed = new MessageEmbed()
       .setAuthor(`Random Icon`)
       .setColor("#16b5ff")
-      .setImage(`http://ddragon.leagueoflegends.com/cdn/11.10.1/img/profileicon/${randIndex}.png`);
+      .setImage(`http://ddragon.leagueoflegends.com/cdn/11.10.1/img/profileicon/${iconID}.png`);
       
     await msg.channel.send(embed);
   }
