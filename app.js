cb5d42777efa4458b9b8b23c0cf34cf65f83e350
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-05-12 04:32:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ client.on("message", async (msg) => {
       `https://anusic-api.herokuapp.com/api/v1/anime/${randSong}`
     ).then((response) => response.json());
     msg.channel.send(randSong);
-    let result = res["data"]["collections"]["themes"]["0"]["sources"]["0"]["link"];
+    let result = res["data"]["collections"]["0"]["themes"]["0"]["sources"]["0"]["link"];
     if(result === undefined){
       msg.channel.send("No results found from rngsus 🙏");
     }else{
