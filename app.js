9857a2e38de99d6381759304f495add4aa8d0ae3
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-05-12 04:27:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -103,6 +103,13 @@ client.on("message", async (msg) => {
     ).then((response) => response.json());
     msg.channel.send(file);
   }
+  if (/ganisong/.test(msg.content.toLowerCase())) {
+    let randSong = Math.floor((Math.random() * 3000) + 1);
+    const res = await fetch(
+      `https://anusic-api.herokuapp.com/api/v1/anime/${randSong}`
+    ).then((response) => response.json());
+    msg.channel.send(res["data"]["collections"]["themes"]["0"]["sources"]["0"]["link"]);
+  }
   if (/gdog/.test(msg.content.toLowerCase())) {
     const res = await fetch(
       "https://dog.ceo/api/breeds/image/random"
