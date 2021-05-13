837db5a6abd35fd28ce80f00f5ed5bf267cd547e
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-05-13 03:10:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ client.on("message", async (msg) => {
     ).then((response) => response.json());
     //after = r['data']['after'];
     
-    msg.channel.send(r['data']['children']['5']['data']['subreddt']);
+    msg.channel.send(r['data']['children']['5']['data']['subreddit']);
     //query = `https://www.reddit.com/r/GenshinImpactNSFW/top.json?sort=top&show=all&t=all&after=${after}`
     //const r = await fetch(
    //   `${query}`
