8c8dbd9b67b2ea902434b4cf3e10c15344af2bcf
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-06-02 14:15:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -170,11 +170,13 @@ client.on("message", async (msg) => {
 
   if (/gmoney/.test(msg.content.toLowerCase())) {
     msg.channel.send(msg.author.id);
+
     const newGamba = new gambaSchema({
       _id: mongoose.Types.ObjectId(),
       userID: msg.author.id,
       money: "100"
     });
+    
     msg.channel.send("Schema created");
     newGamba.save().then(result => msg.channel.send(result));
     msg.channel.send("Made it to the end of gmoney.");
