3ebcc59f03f30063b51c1844ecea24c53b08ae6b
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-06-03 03:33:37 -0300

    update

--- commands/api/cat.js
+++ commands/api/cat.js
@@ -1,8 +1,6 @@
 module.exports = {
   name: "cat",
   description: "Gives a random image of a cat.",
-  aliases: ["cat"],
-  usage: "[cat]",
   async execute(message) {
     const { file } = await fetch("https://aws.random.cat/meow").then(
       (response) => response.json()
