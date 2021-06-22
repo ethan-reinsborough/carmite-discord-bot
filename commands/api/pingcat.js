dac77a3dc3559f693dbce4bbdd70ff77ac8300d1
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-06-22 19:11:10 -0300

    Create pingcat.js

---
+++ commands/api/pingcat.js
@@ -0,0 +1,9 @@
+
+    module.exports = {
+        name: "pc",
+        description: "PING CAT",
+        async execute(message) {
+          message.channel.send("<@!461140829889626123>" + " " + "<:christianserver:830284378180681798>");
+        },
+      };
+      
