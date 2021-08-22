920b4cf52c970ca119c552d2a496c9c7aaf753b2
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-08-22 17:08:29 -0300

    Update mrscha.js

--- commands/games/mrscha.js
+++ commands/games/mrscha.js
@@ -29,17 +29,18 @@ module.exports = {
             .setColor("#16b5ff")
             .setAuthor(`Nice to meet you ${username}! Your journey is about to start!`);
             message.channel.send(embed);
+            const embed = new MessageEmbed()
+          .setDescription("Welcome to the quiet neighborhood of Bottville. You moved here alone after your parents kicked you out for wasting all your time on League of Legends. Your aunt Mrs. Cha pitied you and decided to shelter your poor ass. This is where your story begins.")
+          .setColor("#16b5ff")
+          .setImage('https://i.imgur.com/Nn0W2wz.png')
+         await message.channel.send(embed);
           })
           .catch(() => {
             message.channel.send(
               `❌ | You did not answer in time.`
             );
           });
-          const embed = new MessageEmbed()
-          .setDescription("Welcome to the quiet neighborhood of Bottville. You moved here alone after your parents kicked you out for wasting all your time on League of Legends. Your aunt Mrs. Cha pitied you and decided to shelter your poor ass. This is where your story begins.")
-          .setColor("#16b5ff")
-          .setImage('https://i.imgur.com/Nn0W2wz.png')
-         await message.channel.send(embed);
+          
     },
   };
   
