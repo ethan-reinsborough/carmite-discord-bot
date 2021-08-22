ed3ab936d3a7c2fee4f5b395a20bdad15cbd5ea5
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-08-22 16:50:22 -0300

    Update mrscha.js

--- commands/games/mrscha.js
+++ commands/games/mrscha.js
@@ -1,19 +1,21 @@
 const { MessageEmbed } = require("discord.js");
-
 module.exports = {
     name: "mrscha",
     description: "Oh god why did I make this.",
     async execute(message) {
-        //const filter = (m) => m.author.id === msg.author.id;
-        message.channel.send("HI");
+        const filter = (m) => m.author.id === msg.author.id;
+        var username;
+        /*
         const embed = new MessageEmbed()
-          .setAuthor(`Mrs Cha Test`)
+          .setDescription("")
           .setColor("#16b5ff")
           .setImage('https://i.imgur.com/QZUVE1P.png')
-          .setFooter("Helloooo");
-    
+        */
+          const embed = new MessageEmbed()
+          .setColor("#16b5ff")
+          .setImage('https://i.imgur.com/QZUVE1P.png')
         await message.channel.send(embed);
-        /*
+        
         msg.channel
           .awaitMessages(filter, {
             max: 1,
@@ -22,20 +24,14 @@ module.exports = {
           })
           .then((collected) => {
             const m = collected.first();
-            if (
-              !m.content ||
-              m.content.toLowerCase() !== abilityName.toLowerCase() 
-            )
-              return msg.channel.send(
-                `❌ | Incorrect guess! The answer was **${abilityName}**.`
-              );
-            return msg.channel.send(`✅ | Correct guess!`);
+            username = m.content;
+            return msg.channel.send(`Nice to meet you ${username}!`);
           })
           .catch(() => {
             msg.channel.send(
-              `❌ | You did not answer in time. The correct answer was **${abilityName}**!`
+              `❌ | You did not answer in time.`
             );
-          });*/
+          });
     },
   };
   
