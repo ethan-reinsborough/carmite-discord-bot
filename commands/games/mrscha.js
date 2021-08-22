e7f65bb8888d9707373680d4b62bad362f0b61fe
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-08-22 17:25:17 -0300

    Update mrscha.js

--- commands/games/mrscha.js
+++ commands/games/mrscha.js
@@ -40,12 +40,13 @@ module.exports = {
               `❌ | You did not answer in time.`
             );
           });
-          //next embed goes here
-          const embed2 = new MessageEmbed()
-          .setDescription("Welcome to the quiet neighborhood of Bottville. You moved here alone after your parents kicked you out for wasting all your time on League of Legends. Your aunt Mrs. Cha pitied you and decided to take you in. This is where your story begins.")
+          const embed3 = new MessageEmbed()
+          .setDescription("You walk into the kitchen and come across Mrs. Cha. She looks happy to see you.")
           .setColor("#16b5ff")
-          .setImage('https://i.imgur.com/Nn0W2wz.png');
-         message.channel.send(embed2);
+          .setImage('https://i.imgur.com/NPFPIi1.png');
+         message.channel.send(embed3);
+         embed3.setDescription("Updated");
+         message.channel.edit(embed3);
     },
   };
   
