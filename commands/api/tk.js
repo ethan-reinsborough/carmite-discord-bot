0b6e9aa037f0ed305b3023ce1e72878e3937d28b
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-09-04 16:11:55 -0300

    Update tk.js

--- commands/api/tk.js
+++ commands/api/tk.js
@@ -6,6 +6,9 @@
             let r = await fetch(
                 `https://www.reddit.com/r/knitting/top.json?sort=top&show=all&t=all&after=${global.after}`
               ).then((response) => response.json());
+              msg.channel.send("after = " + global.after);
+              msg.channel.send("counter = " + global.counter);
+              msg.channel.send("page = " + global.page);
               global.counter++;
               if (global.counter > 24) {
                 global.after = r["data"]["after"];
