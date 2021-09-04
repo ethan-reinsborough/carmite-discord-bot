f97806d6148906fdef7dc356c594918da52d4606
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-09-03 22:25:21 -0300

    Create spamstari.js

---
+++ commands/api/spamstari.js
@@ -0,0 +1,9 @@
+
+    module.exports = {
+        name: "ss",
+        description: "SPAM STARI",
+        async execute(message) {
+          message.channel.send("<@!475102784987856896>" + " " + ":sheep:");
+        },
+      };
+      
