8ffc2e26736e33b131cb2ab114488f638a9bc9de
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-09-20 19:40:59 -0300

    Create britney.js

---
+++ commands/api/britney.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "br",
+    description: "1 million lux points!!",
+    async execute(message) {     
+      message.channel.send("https://i.imgur.com/ZVlHJm7.png");
+    },
+  };
+  
