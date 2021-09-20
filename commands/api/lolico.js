fb267533a0ab7ab96a0793538695a44b61714039
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-09-20 17:59:49 -0300

    Create lolico.js

---
+++ commands/api/lolico.js
@@ -0,0 +1,24 @@
+
+    const { MessageEmbed } = require("discord.js");
+  
+  module.exports = {
+    name: "lolico",
+    description: "LOL ICON WOO",
+    async execute(msg) {
+        const res = await fetch(
+            `http://ddragon.leagueoflegends.com/cdn/11.10.1/data/en_US/item.json`
+          ).then((response) => response.json());
+          const keys = Object.keys(res["data"]);
+          const randIndex = Math.floor(Math.random() * keys.length);
+          const iconID = keys[randIndex];
+          const embed = new MessageEmbed()
+            .setAuthor(`Random Icon`)
+            .setColor("#16b5ff")
+            .setImage(
+              `http://ddragon.leagueoflegends.com/cdn/11.10.1/img/profileicon/${iconID}.png`
+            );
+      
+          await msg.channel.send(embed);
+    },
+  };
+  
