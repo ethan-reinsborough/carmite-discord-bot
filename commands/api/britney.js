f25b00add6940a96faa903ccace5b3758463f16a
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-10-03 00:11:05 -0300

    Update britney.js

--- commands/api/britney.js
+++ commands/api/britney.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "br",
     description: "1 million lux points!! dumb very dumb yes",
     async execute(message) {     
-      message.channel.send("https://i.imgur.com/ZVlHJm7.png");
+      message.channel.send("https://i.imgur.com/IsnRk6C.png");
     },
   };
   
