0dfd741c09b88f0bce6b866fb04c4520236a0926
ethan-reinsborough <ethan.reinsborough@gmail.com>
2021-11-25 12:06:23 -0400

    Update a.js

---
+++ commands/utility/a.js
@@ -0,0 +1,46 @@
+module.exports = {
+    name: "a",
+    description: "ethan/ayumi tft duo mode command for items",
+    async execute(message) {
+        input = message.content.split(" ");
+
+        if(input[1] == null || input[1].length > 2){
+            message.channel.send("Usage: s = sword, t = tear, r = rod, v = vest, b = bow, gb = giants belt, g = glove, c = cloak");
+            return;
+        }
+
+        for (let i = 0; i <= input.length; i++) {
+            if(input[i] == "s"){
+                input[i] = "<:sword:913452398876446741>"
+            }
+            if(input[i] == "t"){
+                input[i] = "<:tear:913452295612694610>"
+            }
+            if(input[i] == "r"){
+                input[i] = "<:rod:913452341225730119>"
+            }
+            if(input[i] == "v"){
+                input[i] = "<:vest:9134523879251231129>"
+            }
+            if(input[i] == "b"){
+                input[i] = "<:bow:913452318173851698>"
+            }
+            if(input[i] == "gb"){
+                input[i] = "<:giants:913452364265058314>"
+            }
+            if(input[i] == "g"){
+                input[i] = "<:gloves:913452308493381632>"
+            }
+            if(input[i] == "c"){
+                input[i] = "<:cloak:913452327590064138>"
+            }
+        } 
+        message.delete();
+        var opt1 = input[1];
+        var opt2 = input[2];
+        message.channel.send("<@!189211716607803392>").then(sentEmbed => {
+            sentEmbed.react(opt1);
+            sentEmbed.react(opt2);
+        })
+    },
+  };
