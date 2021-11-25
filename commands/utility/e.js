ede24859fe462e19889240e21b573c8cfb45d5ac
ethan-reinsborough <ethan.reinsborough@gmail.com>
2021-11-25 11:39:32 -0400

    update

---
+++ commands/utility/e.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "e",
+    description: "e/a tft duo mode command for items",
+    async execute(message) {
+        //input = message.content.split(" ");
+        message.channel.send("<:sword:913452398876446741>");
+    },
+  };
