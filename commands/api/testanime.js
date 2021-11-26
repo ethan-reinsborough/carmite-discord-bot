a311226df6f0ce03212408e499256395e12a01af
ethan-reinsborough <ethan.reinsborough@gmail.com>
2021-11-26 13:32:39 -0400

    test

---
+++ commands/api/testanime.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "testanime",
+    description: "Test",
+    async execute(message) {
+      const animeList = await fetch(
+        `https://themes.moe/list/popular/100`
+      ).then((response) => response.json());
+  
+      message.channel.send(result);
+    },
+  };
+  
