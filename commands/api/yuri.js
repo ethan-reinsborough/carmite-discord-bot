8f6ec5c582dd241395e2740e0e8e822afa7ddcc9
ethan-reinsborough <ethan.reinsborough@gmail.com>
2021-11-26 10:14:20 -0400

    Update

--- commands/api/yuri.js
+++ commands/api/yuri.js
@@ -1,7 +1,7 @@
 const hmtai = require("hmtai");
 
 module.exports = {
-    name: "nyuri",
+    name: "yuri",
     description: "Gives a nsfw yuri image.",
     async execute(message) {
         message.channel.send(hmtai.nsfw.yuri());
