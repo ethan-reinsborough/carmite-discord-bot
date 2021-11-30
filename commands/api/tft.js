e9cab080591e9c452ba7b6affb7de1bb57227768
ethan-reinsborough <ethan.reinsborough@gmail.com>
2021-11-30 15:39:17 -0400

    Update tft.js

--- commands/api/tft.js
+++ commands/api/tft.js
@@ -8,8 +8,8 @@ module.exports = {
     async execute(message) {
         input = message.content.split(" ");
 
-        if(input[1] == null || input[2] == null){
-            message.channel.send("Usage: tft <summoner name> <number of matches to show>");
+        if(input[1] == null || input[2] == null || parseInt(input[2]) > 5 || isNaN(input[2])){
+            message.channel.send("Usage: tft <summoner name> <number of matches to show (1-5)>");
             return;
         }
 
