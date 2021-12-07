0a19f91aed50019e2085542997ccc659f3cd69d1
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-12-06 23:50:33 -0400

    yes

--- WIP/pingcat.js
+++
@@ -1,9 +0,0 @@
-
-    module.exports = {
-        name: "pc",
-        description: "PING CAT",
-        async execute(message) {
-          message.channel.send("<@!461140829889626123>" + " " + "<:christianserver:830284378180681798>");
-        },
-      };
-      
