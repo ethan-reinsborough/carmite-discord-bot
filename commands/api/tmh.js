75e6ad0d97927e831547f47d403b1f2aaae0dd82
ethan-reinsborough <ethan.reinsborough@gmail.com>
2021-12-07 10:21:20 -0400

    Update tmh.js

--- commands/api/tmh.js
+++ commands/api/tmh.js
@@ -74,13 +74,16 @@ module.exports = {
             var matchType = "";
             var placement = "";
             for(let y = 0; y < 7; y++){
+                //Incredibly monkey code way of determining your duo in double up using hex codes to add to the chaos
                 var duo = "";
                 if(String(match["info"]["participants"][y]["puuid"]) == String(puuid)){
                     var duo = match["info"]["participants"][y]["placement"];
                     var doublePlacement = GetDoubleUpPlacement(duo);
+                    message.channel.send(doublePlacement);
                 }
                 if(doublePlacement == "#ffcb3d"){
                     if((match["info"]["participants"][y]["placement"] == 1 && String(match["info"]["participants"][y]["puuid"]) !== String(puuid)) || (match["info"]["participants"][y]["placement"] == 2 && String(match["info"]["participants"][y]["puuid"]) !== String(puuid))){
+                        message.channel.send("This should mean my duo is in here!");
                         duo = await fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${match["info"]["participants"][y]["puuid"]}?api_key=${process.env.RIOT_API_KEY}`).then(
                             (response) => response.json());
                     }
