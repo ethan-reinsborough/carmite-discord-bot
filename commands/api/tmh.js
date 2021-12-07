e246b4940b7e01ec2e1e3dbbc8eafb48e8e18df2
ethan-reinsborough <ethan.reinsborough@gmail.com>
2021-12-07 10:35:47 -0400

    Update tmh.js

--- commands/api/tmh.js
+++ commands/api/tmh.js
@@ -73,21 +73,19 @@ module.exports = {
             var matchStats = "";
             var matchType = "";
             var placement = "";
+            var duo = "";
             for(let y = 0; y < 7; y++){
                 if (match["info"]["tft_game_type"] == "pairs") {
                     //Incredibly monkey code way of determining your duo in double up using hex codes to add to the chaos
-                    var duo = "";
                     if (String(match["info"]["participants"][y]["puuid"]) == String(puuid)) {
                         var duo = match["info"]["participants"][y]["placement"];
                         var doublePlacement = GetDoubleUpPlacement(duo);
-                        message.channel.send(doublePlacement);
                     }
                     if (doublePlacement == "#ffcb3d") {
                         if ((match["info"]["participants"][y]["placement"] == 1 && String(match["info"]["participants"][y]["puuid"]) !== String(puuid)) || (match["info"]["participants"][y]["placement"] == 2 && String(match["info"]["participants"][y]["puuid"]) !== String(puuid))) {
                             message.channel.send(match["info"]["participants"][y]["puuid"]);
                             duo = await fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${match["info"]["participants"][y]["puuid"]}?api_key=${process.env.RIOT_API_KEY}`).then(
                                 (response) => response.json());
-                            message.channel.send(duo["name"])
                         }
                     }
                     if (doublePlacement == "#d4d4d4") {
