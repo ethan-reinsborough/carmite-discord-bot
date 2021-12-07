ae07e1e090620ad208091935c135a9b323f81d22
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-12-07 00:19:22 -0400

    Update canvas.js

--- commands/api/canvas.js
+++ commands/api/canvas.js
@@ -6,13 +6,15 @@ module.exports = {
     name: "canvas",
     description: "Test",
     async execute(message) {
-        const canvas = Canvas.createCanvas(400, 400)
+        const canvas = Canvas.createCanvas(700, 500)
         const context = canvas.getContext('2d')    // Load the background image and draw it to the canvas
         const background = await Canvas.loadImage("https://i.imgur.com/ruMzelh.jpeg");
-        let x = 0
-        let y = 0
         context.drawImage(background, 0, 0, canvas.width, canvas.height);
-
+        context.strokeRect(0, 0, canvas.width, canvas.height);
+        context.strokeRect(140, 0, canvas.width, canvas.height);
+        context.strokeRect(280, 0, canvas.width, canvas.height);
+        context.strokeRect(420, 0, canvas.width, canvas.height);
+        context.strokeRect(560, 0, canvas.width, canvas.height);
 	// Use the helpful Attachment class structure to process the file for you
 	const attachment = new MessageAttachment(canvas.toBuffer(), 'profile-image.png');
   const embed = new MessageEmbed()
