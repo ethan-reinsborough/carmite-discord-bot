ab191acfcdd9fab987b2bce3c9568566483ee305
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-12-07 01:58:58 -0400

    Update tmh.js

--- commands/api/tmh.js
+++ commands/api/tmh.js
@@ -177,7 +177,7 @@ module.exports = {
     }
   }
   function timeConverter(UNIX_timestamp){
-    var a = new Date(UNIX_timestamp);
+    var a = new Date(UNIX_timestamp * 1000);
     var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
     var year = a.getFullYear();
     var month = months[a.getMonth()];
