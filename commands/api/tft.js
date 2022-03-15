bf56abedf1317e65569ae042abde9e2cfecbec3f
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2022-03-15 01:05:05 -0300

    Update tft.js

--- commands/api/tft.js
+++ commands/api/tft.js
@@ -131,19 +131,16 @@ module.exports = {
                         var champName2 = `${(match["info"]["participants"][y]["units"][w]["character_id"]).toLowerCase()}` + "_mobile.tft_set6.png";
                         var champImage;
                         try{
-                            message.channel.send(champName);
                             champImage = await Canvas.loadImage(`https://raw.communitydragon.org/pbe/game/assets/ux/tft/championsplashes/${champName}`);
                             context.drawImage(champImage, xcord, ycord, 130, 140);
                         } catch(error){
                             //fuck u jinx
                             if(/jinx/.test(champName2)){
                                 champName2 = "tft6b_jinx_mobile.png";
-                                message.channel.send("Cringe detected.");
                             }
                             if(/vi/.test(champName2)){
                                 champName2 = "tft6b_vi_mobile.png";
                             }
-                            message.channel.send(champName2);
                             champImage = await Canvas.loadImage(`https://raw.communitydragon.org/pbe/game/assets/ux/tft/championsplashes/${champName2}`);
                             context.drawImage(champImage, xcord, ycord, 130, 140);
                         }
