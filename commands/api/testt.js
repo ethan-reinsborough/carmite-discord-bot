971f2a16eafe958dc4a3d2cb9c02e242295e91f3
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2022-07-18 23:27:41 -0300

    Create testt.js

---
+++ commands/api/testt.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: "testt",
+    description: "Test",
+    async execute(message) {
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageSelectMenu()
+                    .setCustomId('select')
+                    .setPlaceholder('Nothing selected')
+                    .addOptions([
+                        {
+                            label: 'Select me',
+                            description: 'This is a description',
+                            value: 'first_option',
+                        },
+                        {
+                            label: 'You can select me too',
+                            description: 'This is also a description',
+                            value: 'second_option',
+                        },
+                    ]))
+                message.channel.send(row);
+    },
+};
