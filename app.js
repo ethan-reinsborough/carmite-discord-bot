d43c848b0a360039a3089753d0daebba703800c9
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2022-10-01 16:05:55 -0700

    Update app.js

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ client.on("message", async (message) => {
   }
 */
   if (!message.content.startsWith(prefix)) return;
-  
+  message.channel.send("I DIDNT EVEN MAKE IT")
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
   message.channel.send("COMMAND NAME: " + commandName);
