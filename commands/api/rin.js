200c916ef1d366437e6ddaac868196ee385de2ae
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2022-10-01 22:48:26 -0700

    werwer

---
+++ commands/api/rin.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "rin",
+    description: "When I'm jellyfishing.",
+    async execute(message) {
+      message.channel.send("https://www.youtube.com/watch?v=XTwkTVjbn78&ab_channel=YourBoySponge" + " 😎");
+    },
+  };
+  
