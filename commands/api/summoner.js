133662709bb79f01abc3fe7e15a1247ea0d95ec5
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2022-10-09 00:56:59 -0700

    Update summoner.js

--- commands/api/summoner.js
+++ commands/api/summoner.js
@@ -5,8 +5,8 @@ const Canvas = require('canvas');
 const { MessageAttachment } = require('discord.js');
 
 module.exports = {
-    name: "tft",
-    description: "TFT MATCH HISTORY NEW!!! Usage -> ;tft <summoner name> <number of matches to show>.",
+    name: "summoner",
+    description: "SUMMONER MATCH HISTORY NEW!!! Usage -> ;tft <summoner name> <number of matches to show>.",
     async execute(message) {
         input = message.content.split(" ");
 
